d39ab94fd6101f2b9c9e64f4f24e5431310f994a
Sumit <sumitgirwals@gmail.com>
2023-07-11 10:31:13 +0530

    Added about and brands section and footer

--- superdoom/src/App.js
+++ superdoom/src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import NotFound404 from "./components/NotFound404";
  
 import "./styles/App.scss";
 import "./styles/header.scss";
+import "./styles/footer.scss";
+import "./styles/home.scss";
 
 function App() {
   return (
@@ -16,6 +19,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound404 />} />
       </Routes>
+      <Footer/>
      </Router>
   );
 }
--- superdoom/src/components/Home.jsx
+++ superdoom/src/components/Home.jsx
@@ -1,15 +1,71 @@
 import React from 'react'
- 
+import img2 from '../assets/images/img2.webp'
+import {
+  AiFillGoogleCircle, 
+  AiFillAmazonCircle, 
+  AiFillYoutube, 
+  AiFillInstagram
+} from "react-icons/ai";
+
 
 function Home() {
-
-     
   return (
-    <div>Home
-
-
-         
+    <>
+    <div className='home' id='home'>
+      <main>
+        <h1>SuperDoom</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </main>
     </div>
+    <div className="home2">
+      <img src={img2} alt="Graphics Image" />
+      <div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab distinctio similique nobis doloremque inventore, temporibus vero ex. Molestiae, modi velit iure dignissimos a at necessitatibus nihil ex quae neque vel!</p>
+      </div>
+    </div>
+
+    <div className="home3" id="about">
+      <div>
+        <h1>Who we are ?</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatum amet perspiciatis iure reiciendis, eius deleniti animi illo modi, eligendi reprehenderit nesciunt sequi ipsam expedita beatae magnam enim delectus tenetur.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatum amet perspiciatis iure reiciendis, eius deleniti animi illo modi, eligendi reprehenderit nesciunt sequi ipsam expedita beatae magnam enim delectus tenetur.</p>
+      </div>
+    </div>
+
+    <div className="home4" id="brands">
+      <div>
+        <h1>Brands</h1>
+         <article>
+          <div style={{
+            animationDelay: '0.3s'
+          }}>
+            <AiFillGoogleCircle />
+            <p>Google</p>
+          </div>
+
+          <div style={{
+            animationDelay: '0.5s'
+          }}>
+            <AiFillAmazonCircle />
+            <p>Amazon</p>
+          </div>
+
+          <div style={{
+            animationDelay: '0.7s'
+          }}>
+            <AiFillYoutube />
+            <p>Youtube</p>
+          </div>
+
+          <div style={{
+            animationDelay: '1s'
+          }}>
+            <AiFillInstagram />
+            <p>Instagram</p>
+          </div>
+         </article>
+      </div>
+    </div>
+    </>
   )
 }
 
